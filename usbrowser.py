fe95b13d1dc81605684a3a77227c6af4c886ee36
freefd <fd@freefd.info>
2024-06-29 15:07:39 +0300

    Add D-Bus integration, improve logging, check if selected executable is available

--- usbrowser.py
+++ usbrowser.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python3
-''' URL Specific Browser-launcher '''
+''' URL Specific Browser Launcher '''
 
 from urllib.parse import urlparse
 import subprocess
@@ -7,7 +7,9 @@ import argparse
 import logging
 import syslog
 import pathlib
+import shutil
 import yaml
+import dbus
 
 def http_url(url):
     ''' URL checker '''
@@ -21,54 +23,96 @@ def http_url(url):
     syslog.syslog(syslog.LOG_ERR, f'{__file__}: not an HTTP/HTTPS URL: {url}')
     raise argparse.ArgumentTypeError(f'Not an HTTP/HTTPS URL: {url}')
 
+def dbus_notification(**kwargs: str) -> None:
+    ''' Send Notification over DBUS '''
+
+    item = 'org.freedesktop.Notifications'
+    notify_interface = dbus.Interface(
+        dbus.SessionBus().get_object(item, f'/{item.replace(".", "/")}'), item)
+
+    # https://specifications.freedesktop.org/notification-spec/latest/ar01s09.html#command-notify
+    notify_interface.Notify(
+        'usbrowser', # app_name
+        0, # replaces_id
+        '', # app_icon
+        kwargs['summary'], # summary
+        kwargs['body'], # body
+        [], # actions
+        {"urgency": kwargs['urgency']}, # hints
+        3000
+    )
+
+    logging.info('[USBROWSER] Notification: [%s], [%s], [%s]',
+                    kwargs['urgency'], kwargs['summary'], repr(kwargs['body']))
+
+
 def launch(bw_name, bw_data, url):
     ''' Browser Launcher '''
 
-    cmd = [bw_data['executable']]
-    logging.info('browser = %s, %s', bw_name, bw_data['executable'])
+    if shutil.which(bw_data['executable']):
+        cmd = [bw_data['executable']]
+        logging.info('[USBROWSER] Browser `%s` with executable `%s`',
+                        bw_name, bw_data['executable'])
 
-    change_url = bw_data.get('change_url', {})
-    url_prefix = change_url.get('prefix', '')
-    transform_url = change_url.get('transform_original_url', [])
+        change_url = bw_data.get('change_url', {})
+        url_prefix = change_url.get('prefix', '')
+        transform_url = change_url.get('transform_original_url', [])
 
-    for transform in transform_url:
-        if transform.get('from', '') != '' and transform.get('to', '') != '':
-            url = url.replace(transform.get('from', ''), transform.get('to', ''))
+        for transform in transform_url:
+            if (transform.get('from', '') != ''
+                and transform.get('to', '') != ''):
+                url = url.replace(
+                    transform.get('from', ''),
+                    transform.get('to', ''))
 
-    if url_prefix != '':
-        url = url_prefix + url
+        if url_prefix != '':
+            url = url_prefix + url
 
-    logging.info('url = %s', url)
+        logging.info('[USBROWSER] URL is `%s`', url)
 
-    if 'arguments_list' in bw_data and len(bw_data['arguments_list']) > 0:
-        for argument_key, argument_value in bw_data['arguments_list'].items():
-            if bw_data['arguments_delimiter']:
-                argument_delimiter = bw_data['arguments_delimiter']
-            else:
-                argument_delimiter = ''
+        if 'arguments_list' in bw_data and len(bw_data['arguments_list']) > 0:
+            for arg_key, arg_value in bw_data['arguments_list'].items():
+                if bw_data['arguments_delimiter']:
+                    argument_delimiter = bw_data['arguments_delimiter']
+                else:
+                    argument_delimiter = ''
 
-            logging.info( 'arguments_list element = %s%s%s',
-                argument_key, argument_delimiter, argument_value)
+                logging.info(
+                    '[USBROWSER] Arguments list element is `%s%s%s`',
+                    arg_key, argument_delimiter, arg_value)
 
-            cmd.append(f'{argument_key}{argument_delimiter}{argument_value}')
+                cmd.append(
+                    f'{arg_key}{argument_delimiter}{arg_value}')
 
-    cmd.append(url)
+        cmd.append(url)
 
-    logging.info('command = %s', ' '.join(cmd))
+        logging.info('[USBROWSER] Command is `%s`', ' '.join(cmd))
 
-    try :
-        subprocess.check_call(cmd)
-    except subprocess.CalledProcessError:
-        syslog.syslog(
-            syslog.LOG_ERR,
-            f'{__file__}: could not open URL with browser '
-            f'{bw_name}: {url}'
+        try:
+            subprocess.Popen(cmd, start_new_session=True)
+        except subprocess.CalledProcessError:
+            syslog.syslog(
+                syslog.LOG_ERR,
+                f'{__file__}: could not open URL with browser '
+                f'{bw_name}: {url}'
+            )
+            raise
+    else:
+        dbus_notification(
+            urgency = 2,
+            summary = 'URL Specific Browser-launcher',
+            body = f'Cannot find executable `{bw_data["executable"]}` '
+                   f'to launch {url}'
         )
-        raise
+        logging.critical('[USBROWSER] Cannot find executable `%s`',
+                bw_data['executable'])
+        syslog.syslog(syslog.LOG_ERR,
+                f'{__file__}: Cannot find executable `{bw_data["executable"]}`'
+            )
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-        description='Handler for http/https URLs.',
+        description='URL Specific Browser Launcher',
     )
     parser.add_argument(
         '-v',
@@ -92,15 +136,17 @@ if __name__ == '__main__':
         type=http_url,
         help="URL starting with 'http://' or 'https://'",
     )
+
+    config = {"default":{"executable": "chromium"}}
+
     args = parser.parse_args()
     logging.basicConfig(level=args.loglevel)
 
     parsed = urlparse(args.url)
-    logging.debug(parsed)
-    config = {"default":{"executable": "chromium"}}
     PARSED_PATH = '/' if parsed.path == '' else parsed.path
-    logging.debug('Setting default config')
-    logging.debug('Reading system and user config files')
+
+    logging.debug('[USBROWSER] Setting default config:\n%s', yaml.dump(config, indent=2))
+    logging.debug('[USBROWSER] Reading system and user config files')
 
     paths = [
         '/etc/usbrowser.yaml',
@@ -109,31 +155,32 @@ if __name__ == '__main__':
     for path in paths:
         try:
             with open(path, 'r', encoding="utf-8") as file:
-                logging.debug('Found config file: %s', path)
+                logging.debug('[USBROWSER] Found config file: %s', path)
 
                 data = yaml.safe_load(file)
-                logging.debug('Data: %s', data)
+                logging.debug('[USBROWSER] Data:\n%s', yaml.dump(data, indent=2))
 
                 for browser_name, browser_data in data.items():
                     logging.debug(
-                        'Adding %s config from that config file', browser_name)
+                        '[USBROWSER] Adding `%s` config from `%s` config file',
+                        browser_name, path)
 
                     if browser_name not in config:
-                        logging.debug('New browser - all items added')
+                        logging.debug('[USBROWSER] New browser - all items added')
                         config[browser_name] = browser_data
                     else:
                         for match_url_name, match_url_list in browser_data.items():
                             logging.debug(
-                                'Add match_url details %s to that browser',
-                                match_url_name
-                            )
+                                '[USBROWSER] Add match_url details '
+                                '`%s` to `%s` browser',
+                                match_url_name, browser_name)
                             config[browser_name][
                                 match_url_name] = match_url_list
 
         except FileNotFoundError:
-            logging.debug('Unable to find config file: %s', path)
+            logging.debug('[USBROWSER] Unable to find config file: %s', path)
 
-    logging.debug('Config files read - Final result: %s', config)
+    logging.debug('[USBROWSER] Config files read - Final result:\n%s', yaml.dump(config, indent=2))
 
     FOUND_BROWSER = False
     for browser_name, browser_data in config.items():
@@ -141,13 +188,15 @@ if __name__ == '__main__':
             break
 
         logging.debug(
-            'Checking to see if %s matches the supplied path', browser_name)
+            '[USBROWSER] Checking to see if `%s` matches the supplied path',
+            browser_name)
         match_urls = browser_data.get('urls', {})
         for match_url_name, match_url_list in match_urls.items():
             if FOUND_BROWSER:
                 break
 
-            logging.debug('Parsing the list of %s urls', match_url_name)
+            logging.debug('[USBROWSER] Parsing the list of `%s` urls',
+                            match_url_name)
 
             for match_url in match_url_list:
                 if FOUND_BROWSER:
@@ -160,14 +209,15 @@ if __name__ == '__main__':
                 match_url_path_start = '/' + match_url.get('path_start', '')
 
                 logging.debug(
-                    'Checking to see whether the scheme of %s matches %s',
+                    '[USBROWSER] Checking to see whether the '
+                    'scheme of `%s` matches `%s`',
                     match_url_scheme, parsed.scheme
                 )
 
                 if match_url_scheme in ('..EMPTY..', parsed.scheme):
                     logging.debug(
-                        'Matching domain %s with match_url_domain %s'
-                        ' or match_url_domain_end %s',
+                        '[USBROWSER] Matching domain `%s` with '
+                        'match_url_domain `%s` or match_url_domain_end `%s`',
                         parsed.netloc, match_url_domain, match_url_domain_end
                     )
 
@@ -176,16 +226,18 @@ if __name__ == '__main__':
                         (match_url_domain == '..EMPTY..'
                             and match_url_domain_end == '..EMPTY..')):
                         logging.debug(
-                            'Matching path %s with match_url_path_start %s',
+                            '[USBROWSER] Matching path `%s` with '
+                            'match_url_path_start `%s`',
                             PARSED_PATH, match_url_path_start
                         )
 
                         if PARSED_PATH.startswith(match_url_path_start):
                             logging.debug(
-                                'Found a match. Ceasing any further checks.')
+                                '[USBROWSER] Found a match. Ceasing '
+                                'any further checks.')
                             FOUND_BROWSER = True
                             launch(browser_name, browser_data, args.url)
 
     if not FOUND_BROWSER:
-        logging.debug('Using default browser')
+        logging.debug('[USBROWSER] Using default browser')
         launch("default", config["default"], args.url)
